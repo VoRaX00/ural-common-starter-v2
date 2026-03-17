3c5da2adc91e8f028f0fa0a01e4e09745d76bdb6
kerzhakov <kerzhakov@atb.su>
2026-03-18 00:24:20 +1000

    added new exception

---
+++ ural-exception-starter/src/main/java/ural/ru/exceptions/ConflictException.java
@@ -0,0 +1,21 @@
+package ural.ru.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ConflictException extends RestBaseException {
+    public ConflictException(String message) {
+        super(message, HttpStatus.CONFLICT);
+    }
+
+    public ConflictException(String message, Throwable throwable) {
+        super(message, HttpStatus.CONFLICT, throwable);
+    }
+
+    public ConflictException(String message, HttpStatus httpStatus) {
+        super(message, httpStatus);
+    }
+
+    public ConflictException(String message, HttpStatus httpStatus, Throwable throwable) {
+        super(message, httpStatus, throwable);
+    }
+}
