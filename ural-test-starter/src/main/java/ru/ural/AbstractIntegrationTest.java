0e4402b1bcfefcbd7222edf08065992e733f21bf
kerzhakov <kerzhakov@atb.su>
2026-03-22 11:45:24 +1000

    update ural-test-starter

---
+++ ural-test-starter/src/main/java/ru/ural/AbstractIntegrationTest.java
@@ -0,0 +1,37 @@
+package ru.ural;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.*;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractIntegrationTest {
+
+    private static final String POSTGRES_CONTAINER_IMAGE = "postgres:16.3";
+
+    static final DockerImageName POSTGRES_IMAGE = DockerImageName
+            .parse(POSTGRES_CONTAINER_IMAGE)
+            .asCompatibleSubstituteFor("postgres");
+
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES_CONTAINER = new PostgreSQLContainer<>(POSTGRES_IMAGE)
+            .withDatabaseName("ural")
+            .withUsername("postgres")
+            .withPassword("postgres");
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRES_CONTAINER::getPassword);
+    }
+
+}
